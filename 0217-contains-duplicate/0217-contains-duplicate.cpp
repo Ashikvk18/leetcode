2a1bb2b366f80140153f1e560233c73f68ed7c84
Ashik Dey Rupak <129338031+Ashikvk18@users.noreply.github.com>
2026-06-05 06:11:43 -0500

    Time: 47 ms (68.54%), Space: 90.8 MB (45.27%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        unordered_set<int> s;
+        for(auto& x : nums){
+            if(s.contains(x)){
+                return true;
+            }
+            else{
+                s.insert(x);
+            }
+        }
+        return false;
+        
+    }
+};
