83a54b60940d803ea45a2f11371b8b21b2bc1b50
Ashik Dey Rupak <129338031+Ashikvk18@users.noreply.github.com>
2026-06-10 05:15:03 -0500

    Time: 3 ms (45.89%), Space: 9.9 MB (25.28%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -2,17 +2,22 @@ class Solution {
 public:
     bool isAnagram(string s, string t) {
         unordered_map<char, int> map;
-        if(s.length()!=t.length()){
+        if(s.length()!=t.length())
+        {
             return false;
         }
-        for(auto x : s){
+        for(auto x : s)
+        {
             map[x]++;
         }
-        for(auto x : t){
+        for(auto x : t)
+        {
             map[x]--;
         }
-        for(auto x : map){
-            if(x.second!=0){
+        for(auto x : map)
+        {
+            if(x.second!=0)
+            {
                 return false;
             }
         }
