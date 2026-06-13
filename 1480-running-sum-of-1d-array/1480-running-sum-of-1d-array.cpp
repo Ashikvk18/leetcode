fac2a82380ed6689f1aa7488b66804e5d297ad2f
Ashik Dey Rupak <129338031+Ashikvk18@users.noreply.github.com>
2026-06-13 14:49:54 -0500

    Time: 0 ms (100%), Space: 12.4 MB (98.67%) - LeetHub

--- 1480-running-sum-of-1d-array/1480-running-sum-of-1d-array.cpp
+++ 1480-running-sum-of-1d-array/1480-running-sum-of-1d-array.cpp
@@ -1,9 +1,11 @@
 class Solution {
 public:
-    vector<int> runningSum(vector<int>& nums) {
+    vector<int> runningSum(vector<int>& nums)
+     {
         vector<int> ans = {nums[0]};
-        for(int i = 1; i < nums.size(); i++){
-            ans.push_back(ans[ans.size()-1]+nums[i]);
+        for(int i = 1; i < nums.size(); i++)
+        {
+            ans.push_back(ans[ans.size()-1]+nums[i]); //i applied the prefix sum technique here
         }
         return ans;
     }
