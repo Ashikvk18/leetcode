4dcda7935296c7514e2761c9c1f068a10a67f388
Ashik Dey Rupak <129338031+Ashikvk18@users.noreply.github.com>
2026-06-19 12:40:51 -0500

    Time: 0 ms (100%), Space: 8 MB (53.83%) - LeetHub

--- 0917-reverse-only-letters/0917-reverse-only-letters.cpp
+++ 0917-reverse-only-letters/0917-reverse-only-letters.cpp
@@ -1,26 +1,26 @@
 class Solution {
 public:
-    bool valid(char c){
-        return ('A' <= c && c <= 'Z') || ('a' <= c && c <= 'z');
-    }
-    string reverseOnlyLetters(string s) {
-       int i = 0;
-       int j = s.size()-1;
-       for(;i<j;){
-        if(!valid(s[i])){
-            i++;
-        }
-        else if(!valid(s[j])){
-            j--;
-        }
-        else{
+    string reverseOnlyLetters(string s) {   
+        int i = 0;
+        int j = s.size()-1;
+        while(i<j){
+            if(!valid(s[i])){
+                i++;
+            }
+            else if(!valid(s[j])){
+                j--;
+            }
+            else{
             char temp = s[i];
             s[i] = s[j];
             s[j] = temp;
             i++;
             j--;
+            }
         }
-       }
-       return s;
+        return s;
+    }
+    bool valid(char c){
+        return (('A'<=c && c<='Z')||('a'<=c && c<='z'));
     }
 };
