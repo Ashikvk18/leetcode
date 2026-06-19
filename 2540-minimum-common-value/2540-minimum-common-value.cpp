27eec56f2c840bf5b333a9a58283debac1e7faee
Ashik Dey Rupak <129338031+Ashikvk18@users.noreply.github.com>
2026-06-19 15:38:36 -0500

    Time: 0 ms (100%), Space: 54.4 MB (80.21%) - LeetHub

--- 2540-minimum-common-value/2540-minimum-common-value.cpp
+++ 2540-minimum-common-value/2540-minimum-common-value.cpp
@@ -3,17 +3,19 @@ public:
     int getCommon(vector<int>& nums1, vector<int>& nums2) {
         int i = 0;
         int j = 0;
-        while (i<nums1.size() && j<nums2.size()){
-            if (nums1[i]==nums2[j]){
+
+        while (i < nums1.size() && j < nums2.size()) {
+            if (nums1[i] == nums2[j]) {
                 return nums1[i];
-            }else if(nums1[i]>nums2[j]){
-                while(j < nums2.size() && nums2[j] < nums1[i]){
-                    j++;
-                }}else{
-                    while(i < nums1.size() && nums1[i] < nums2[j]){
-                    i++;}
-                }
+            }
+            else if (nums1[i] < nums2[j]) {
+                i++;
+            }
+            else {
+                j++;
+            }
         }
+
         return -1;
     }
 };
