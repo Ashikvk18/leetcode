93c25541cf39000d36034336c57faf54341b87cc
Ashik Dey Rupak <129338031+Ashikvk18@users.noreply.github.com>
2026-06-23 06:00:40 -0500

    Time: 3 ms (67.05%), Space: 14.8 MB (54.53%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -1,11 +1,14 @@
 class Solution {
 public:
-    vector<int> twoSum(vector<int>& nums, int target) {
+    vector<int> twoSum(vector<int>& nums, int target) 
+    {
         unordered_map<int, int> hmap;
-        for(int i {0uz}; i < nums.size(); i++){
+        for(int i {0uz}; i < nums.size(); i++)
+        {
             int num = nums[i];
             int comp = target - num;
-            if(hmap.contains(comp)){
+            if(hmap.contains(comp))
+            {
                 return {hmap[comp],i};
             }
             hmap[num]=i;
