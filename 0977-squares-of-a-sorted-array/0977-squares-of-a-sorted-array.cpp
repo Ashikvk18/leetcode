40d0b9d57886aa59912552e429d8e84dd45bbecf
Ashik Dey Rupak <129338031+Ashikvk18@users.noreply.github.com>
2026-07-10 21:23:54 -0500

    Time: 0 ms (100%), Space: 30.3 MB (39.96%) - LeetHub

--- 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.cpp
+++ 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.cpp
@@ -4,12 +4,12 @@ public:
         int i = 0;
         int j = nums.size()-1;
         int k = nums.size()-1;
-        vector<int> ans(nums.size());
+        vector<int>ans(nums.size());
         while(i<=j){
             if(abs(nums[i])>abs(nums[j])){
                 ans[k]=nums[i]*nums[i];
                 i++;
-            } else {
+            }else{
                 ans[k]=nums[j]*nums[j];
                 j--;
             }
