8c6b6036cad6dfd800f687b18afb01e492fee74c
Ashik Dey Rupak <129338031+Ashikvk18@users.noreply.github.com>
2026-07-10 21:05:42 -0500

    Time: 0 ms (100%), Space: 27.2 MB (80.95%) - LeetHub

--- 0344-reverse-string/0344-reverse-string.cpp
+++ 0344-reverse-string/0344-reverse-string.cpp
@@ -4,9 +4,9 @@ public:
         int i = 0;
         int j = s.size()-1;
         while(i<j){
-            char t = s[i];
+            char temp = s[i];
             s[i] = s[j];
-            s[j] = t;
+            s[j] = temp;
             i++;
             j--;
         }
