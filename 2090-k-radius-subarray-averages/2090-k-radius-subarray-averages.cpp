86dcbed4d02d0cab4a68d810d9df3548c126904b
Ashik Dey Rupak <129338031+Ashikvk18@users.noreply.github.com>
2026-07-12 11:35:36 -0500

    Time: 0 ms (100%), Space: 134.7 MB (35.75%) - LeetHub

--- 2090-k-radius-subarray-averages/2090-k-radius-subarray-averages.cpp
+++ 2090-k-radius-subarray-averages/2090-k-radius-subarray-averages.cpp
@@ -2,21 +2,24 @@ class Solution {
 public:
     vector<int> getAverages(vector<int>& nums, int k) {
         vector<int>ans(nums.size(),-1);
+        long long sum = 0;
         int window = 2*k+1;
-        long windowsum = 0;
-        if(k<=0){
-            return nums;
-        }
-        if (window > nums.size()){
+        if(k<0){
             return ans;
         }
-        for(int i = 0; i<window; i++){
-             windowsum += nums[i];
+        if(k==0){
+            return nums;
         }
-        ans[k]=windowsum/window;
+        if(window>nums.size()){
+            return ans;
+        }
+        for(int i = 0; i < window; i++){
+            sum += nums[i];
+        }
+        ans[k]=sum/window;
         for(int i = window; i < nums.size(); i++){
-            windowsum = windowsum + nums[i] - nums[i-window];
-            ans[i-k]=windowsum/window;
+            sum = sum + nums[i] - nums[i-window];
+            ans[i-k]=sum/window;
         }
         return ans;
     }
