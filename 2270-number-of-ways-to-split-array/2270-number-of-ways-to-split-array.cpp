a12b4ccac25b3c0df83181ac4b2dd4fa7d83c0d2
Ashik Dey Rupak <129338031+Ashikvk18@users.noreply.github.com>
2026-07-11 19:48:45 -0500

    Time: 0 ms (100%), Space: 89.5 MB (62.32%) - LeetHub

--- 2270-number-of-ways-to-split-array/2270-number-of-ways-to-split-array.cpp
+++ 2270-number-of-ways-to-split-array/2270-number-of-ways-to-split-array.cpp
@@ -1,15 +1,16 @@
 class Solution {
 public:
     int waysToSplitArray(vector<int>& nums) {
-        vector<long long>prefix={nums[0]};
-        for(int i = 1; i < nums.size(); i++){
-            prefix.push_back(prefix.back()+nums[i]);
+        long total = 0;
+        for(int i = 0; i < nums.size(); i++){
+            total += nums[i];
         }
-        long ans = 0;
+        int ans = 0;
+        long left = 0;
         for(int i = 0; i < nums.size()-1; i++){
-            long left = prefix[i];
-            long right = prefix.back()-prefix[i];
-            if(left>=right){
+            left +=nums[i];
+            long right = total - left;
+            if(left >= right){
                 ans++;
             }
         }
