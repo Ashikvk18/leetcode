98838bd278dda5d49dd089bbe0cf52f277ce7d9d
Ashik Dey Rupak <129338031+Ashikvk18@users.noreply.github.com>
2026-07-13 14:04:40 -0500

    Time: 0 ms (100%), Space: 8.4 MB (35.8%) - LeetHub

--- 2000-reverse-prefix-of-word/2000-reverse-prefix-of-word.cpp
+++ 2000-reverse-prefix-of-word/2000-reverse-prefix-of-word.cpp
@@ -3,21 +3,19 @@ public:
     string reversePrefix(string word, char ch) {
         int i = 0;
         int j = 0;
-        while(j<word.size() && word[j]!=ch){
+        while( j < word.size() && word[j]!=ch){
             j++;
         }
-        if(j==word.size()){
+        if(j == word.size()){
             return word;
         }
-        else{
-            while(i<j){
-                char temp = word[i];
-                word[i] = word[j];
-                word[j] = temp;
-                i++;
-                j--;
-            }
-            return word;
+        while(i<j){
+            char temp = word[i];
+            word[i] = word[j];
+            word[j] = temp;
+            i++;
+            j--;
         }
+        return word;
     }
 };
