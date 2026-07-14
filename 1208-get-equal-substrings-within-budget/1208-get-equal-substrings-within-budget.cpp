1c45f1b81c4917e3157dfb1ad71c3db36080b6fb
Ashik Dey Rupak <129338031+Ashikvk18@users.noreply.github.com>
2026-07-14 15:41:41 -0500

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1208-get-equal-substrings-within-budget/1208-get-equal-substrings-within-budget.cpp
+++ 1208-get-equal-substrings-within-budget/1208-get-equal-substrings-within-budget.cpp
@@ -3,15 +3,15 @@ public:
     int equalSubstring(string s, string t, int maxCost) {
         int l = 0;
         int r = 0;
-        int curr = 0;
+        int cost = 0;
         int ans = 0;
         for(; r < s.size(); r++){
-            curr+=abs(s[r]-t[r]);
-            while(curr>maxCost){
-                curr-=abs(s[l]-t[l]);
+            cost+=abs(s[r]-t[r]);
+            while(cost>maxCost){
+                cost-=abs(s[l]-t[l]);
                 l++;
             }
-            ans = max(ans, r-l+1);
+            cost = max(ans, r-l+1);
         }
         return ans;
     }
