0d1561be89e759df7c55321f9a6a3788f0f82383
Ashik Dey Rupak <129338031+Ashikvk18@users.noreply.github.com>
2026-07-15 20:54:36 -0500

    Time: 0 ms (100%), Space: 37.1 MB (5.13%) - LeetHub

--- 0724-find-pivot-index/0724-find-pivot-index.cpp
+++ 0724-find-pivot-index/0724-find-pivot-index.cpp
@@ -1,19 +1,17 @@
 class Solution {
 public:
     int pivotIndex(vector<int>& nums) {
-        vector<int>prefix={nums[0]};
+        vector<int> prefix = {nums[0]};
         for(int i = 1; i < nums.size(); i++){
-            prefix.push_back(nums[i]+prefix.back());
+            prefix.push_back(prefix.back()+nums[i]);
         }
+        int left = 0;
         for(int i = 0; i < nums.size(); i++){
-            int left = 0;
-            if(i>0){
-                 left = prefix[i-1];
-            }
-            int right = prefix.back()-prefix[i];
+            int right = prefix.back()-left-nums[i];
             if(left==right){
                 return i;
             }
+            left +=nums[i];
         }
         return -1;
     }
