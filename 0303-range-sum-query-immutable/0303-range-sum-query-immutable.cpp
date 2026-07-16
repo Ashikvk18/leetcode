a9ea6f12f514f05c45795cbfea8add08dc016926
Ashik Dey Rupak <129338031+Ashikvk18@users.noreply.github.com>
2026-07-15 21:28:21 -0500

    Time: 3 ms (46.69%), Space: 23.9 MB (95.33%) - LeetHub

--- 0303-range-sum-query-immutable/0303-range-sum-query-immutable.cpp
+++ 0303-range-sum-query-immutable/0303-range-sum-query-immutable.cpp
@@ -1,15 +1,18 @@
 class NumArray {
 public:
-    vector<int> prefix;
+    vector<int>prefix;
     NumArray(vector<int>& nums) {
-        prefix = {0};
-        for(int i = 0; i < nums.size(); i++){
-            prefix.push_back(nums[i] + prefix.back());
+        prefix = {nums[0]};
+        for(int i = 1; i < nums.size(); i++){
+            prefix.push_back(prefix.back()+nums[i]);
         }
     }
     
     int sumRange(int left, int right) {
-        return prefix[right+1]-prefix[left];
+        if(left == 0){
+            return prefix[right];
+        }
+        return prefix[right]-prefix[left-1];
     }
 };
 
