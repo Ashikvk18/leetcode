de5e61d20b5f63a22ec1cb991378b53ef0d634ff
Ashik Dey Rupak <129338031+Ashikvk18@users.noreply.github.com>
2026-07-25 19:36:40 -0500

    Time: 0 ms (100%), Space: 21.8 MB (71.36%) - LeetHub

--- 0268-missing-number/0268-missing-number.cpp
+++ 0268-missing-number/0268-missing-number.cpp
@@ -1,12 +1,21 @@
 class Solution {
 public:
-    int missingNumber(vector<int>& nums) {
-        int n = nums.size();
-        int total = (n*(n+1))/2;
-        int numsum = 0;
-        for(int i = 0; i < nums.size(); i++){
-            numsum += nums[i];
-        }
-        return total - numsum;
+    int missingNumber(vector<int>& nums) 
+    {
+        // unordered_set<int>hset(nums.begin(),nums.end());
+    int n = nums.size();
+    int total= n*(n+1)/2;
+    int sum = 0;
+    // for(int i = 0; i<=nums.size(); i++){
+    //    if(!hset.contains(i)){
+    //     return i;
+    //    }
+    // }
+    // return -1;
+    // } Better solution is the math one as no extra space needed
+    for(int i = 0; i<n;i++){
+        sum +=nums[i];
+    }
+    return total - sum;
     }
 };
