11479601a7a647f15344a66ee2efcce4a2b123ae
Ashik Dey Rupak <129338031+Ashikvk18@users.noreply.github.com>
2026-07-25 19:14:26 -0500

    Time: 0 ms (100%), Space: 9.1 MB (6.93%) - LeetHub

--- 1832-check-if-the-sentence-is-pangram/1832-check-if-the-sentence-is-pangram.cpp
+++ 1832-check-if-the-sentence-is-pangram/1832-check-if-the-sentence-is-pangram.cpp
@@ -1,12 +1,10 @@
 class Solution {
 public:
     bool checkIfPangram(string sentence) {
-       unordered_set<char>hset(sentence.begin(),sentence.end());
-       if(hset.size() == 26){
-        return true;
-       } 
-       else{
+        unordered_set<char> hset (sentence.begin(), sentence.end());
+        if(hset.size() == 26){
+            return true;
+        }
         return false;
-       }
     }
 };
