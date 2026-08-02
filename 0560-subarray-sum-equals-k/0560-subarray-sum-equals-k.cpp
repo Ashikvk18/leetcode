00402baa3b1c451e5ad0caaddf39ee681c9eb6ee
Ashik Dey Rupak <129338031+Ashikvk18@users.noreply.github.com>
2026-08-01 21:54:04 -0500

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
@@ -1,15 +1,15 @@
 class Solution {
 public:
     int subarraySum(vector<int>& nums, int k) {
-        unordered_map<int, int>hmap;
         int curr = 0;
-        int ans = 0;
-        hmap[0] = 1;
-        for(int num:nums){
-            curr+=num;
-            ans+=hmap[curr-k];
+        unordered_map<int, int>hmap;
+        hmap[0]=1;
+        int ans=0;
+        for(auto x : nums){
+            curr += x;
             hmap[curr]++;
-        }
-        return ans;
+            ans += hmap[curr-k];
+            }
+            return ans;
     }
 };
