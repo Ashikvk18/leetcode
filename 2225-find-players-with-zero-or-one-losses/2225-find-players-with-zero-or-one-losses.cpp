02556b2f258d35dddeceffd6baf3e66e1b6409d9
Ashik Dey Rupak <129338031+Ashikvk18@users.noreply.github.com>
2026-08-02 15:58:06 -0500

    Time: 194 ms (47.32%), Space: 185.3 MB (31.7%) - LeetHub

---
+++ 2225-find-players-with-zero-or-one-losses/2225-find-players-with-zero-or-one-losses.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    vector<vector<int>> findWinners(vector<vector<int>>& matches) {
+        unordered_map<int, int> hmap;
+        for(auto x : matches){
+            int y = x[0];
+            int z = x[1];
+            if(!hmap.contains(y)){
+                hmap[y]==0;
+            }
+            hmap[z]++;
+        }
+        vector<int>w;
+        vector<int>l;
+        for(auto x : hmap){
+            if(x.second==0){
+                w.push_back(x.first);
+            } else if(x.second==1){
+                l.push_back(x.first);
+            }
+        }
+        sort(w.begin(),w.end());
+        sort(l.begin(),l.end());
+        return {w,l};
+    }
+};
