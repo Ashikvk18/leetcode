e1b25e2a6767e295aff3f35ff522a2a0fa244118
Ashik Dey Rupak <129338031+Ashikvk18@users.noreply.github.com>
2026-08-08 21:44:33 -0500

    Time: 0 ms (100%), Space: 134.8 MB (34.5%) - LeetHub

--- 2090-k-radius-subarray-averages/2090-k-radius-subarray-averages.cpp
+++ 2090-k-radius-subarray-averages/2090-k-radius-subarray-averages.cpp
@@ -1,26 +1,26 @@
 class Solution {
 public:
     vector<int> getAverages(vector<int>& nums, int k) {
-        vector<int>ans(nums.size(),-1);
-        long long sum = 0;
-        int window = 2*k+1;
-        if(k<0){
-            return ans;
-        }
-        if(k==0){
-            return nums;
-        }
-        if(window>nums.size()){
-            return ans;
-        }
-        for(int i = 0; i < window; i++){
-            sum += nums[i];
-        }
-        ans[k]=sum/window;
-        for(int i = window; i < nums.size(); i++){
-            sum = sum + nums[i] - nums[i-window];
-            ans[i-k]=sum/window;
-        }
+    int window = 2*k+1;
+    long sum = 0;
+    vector<int>ans(nums.size(),-1);
+    if(k<0){
         return ans;
+    }
+    if(k == 0){
+        return nums;
+    }
+    if(window>nums.size()){
+        return ans;
+    }
+    for(int i = 0; i < window; i++){
+        sum += nums[i];
+    }
+    ans[k] = sum / window;
+    for(int i = window; i < nums.size(); i++){
+        sum = sum + nums[i] - nums[i-window];
+        ans[i-k] = sum / window;
+    }
+    return ans;
     }
 };
