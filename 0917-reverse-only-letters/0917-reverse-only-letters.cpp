2d7cee9494dde208912a4bbfaf733df9f08bca26
Ashik Dey Rupak <129338031+Ashikvk18@users.noreply.github.com>
2026-09-14 20:35:54 -0500

    Time: 2 ms (2.27%), Space: 8 MB (55.53%) - LeetHub

--- 0917-reverse-only-letters/0917-reverse-only-letters.cpp
+++ 0917-reverse-only-letters/0917-reverse-only-letters.cpp
@@ -3,23 +3,22 @@ public:
     string reverseOnlyLetters(string s) {
         int i = 0;
         int j = s.size()-1;
-        while(i<j){
-            if(!valid(s[i])){
+        while (i<j){
+            while(i<j && !valid(s[i])){
                 i++;
             }
-            else if(!valid(s[j])){
+            while(i<j && !valid(s[j])){
                 j--;
-            }else{
+            }
             char temp = s[i];
             s[i] = s[j];
             s[j] = temp;
             i++;
             j--;
-            }
         }
         return s;
     }
     bool valid(char c){
-        return ('A'<= c && c <= 'Z')||('a'<= c && c <= 'z');
+        return ('A'<=c && c<='Z')||('a'<=c && c<='z');
     }
 };
