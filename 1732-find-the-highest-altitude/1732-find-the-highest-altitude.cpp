a4cc7ecb7f1a76d2b00b0b71efb3b33cb268b7db
Ashik Dey Rupak <129338031+Ashikvk18@users.noreply.github.com>
2026-09-22 04:03:46 -0500

    Time: 0 ms (100%), Space: 10.9 MB (44.63%) - LeetHub

--- 1732-find-the-highest-altitude/1732-find-the-highest-altitude.cpp
+++ 1732-find-the-highest-altitude/1732-find-the-highest-altitude.cpp
@@ -1,9 +1,9 @@
 class Solution {
 public:
     int largestAltitude(vector<int>& gain) {
-        vector<int> prefix = {0};
+        vector< int > prefix = {0};
         int ans = 0;
-        for(int i = 0; i < gain.size(); i++){
+        for(int i {0uz}; i < gain.size(); i++){
             prefix.push_back(prefix.back()+gain[i]);
             ans = max(ans, prefix.back());
         }
